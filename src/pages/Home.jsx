482ab0c2751838e7584bd12daeabab1d2c5c30e2
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-03-02 12:55:50 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,7 +57,7 @@ function Home() {
 				>
 					<p className='text-6xl font-playfair z-10 text-center md:text-start'>
 						Oluwole{' '}
-						<span className='xs:relative xs:text-white xs:font-semibold z-100 xs:before:content-brush before:absolute before:-left-[25px] before:-top-[70px] before:z-[-100]'>
+						<span className=''>
 							Samson
 						</span>
 					</p>
